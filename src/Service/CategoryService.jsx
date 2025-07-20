6552db6b7b13741ebaa38a1025f499a6b1289e9b
abdurrahman2004 <ar978874@gmail.com>
2025-07-20 22:03:23 +0530

    Axios path issued fixed

--- src/Service/CategoryService.jsx
+++ src/Service/CategoryService.jsx
@@ -4,13 +4,13 @@ import React from 'react'
 
 
 export const addCategory = async(category) => {
-  return await  axios.post('http://localhost:8080/api/v1.0/categories',category)
+  return await  axios.post('http://localhost:8080/api/v1.0/admin/categories',category)
 }
 
 export const deleteCategory = async (categoryId) => {
-    return await axios.delete(`http://localhost:8080/api/v1.0/categories/${categoryId}`);
+    return await axios.delete(`http://localhost:8080/api/v1.0/admin/categories/${categoryId}`);
 }
 
 export const fetchCategories = async () => {
-    return await axios.get('http://localhost:8080/api/v1.0/categories');
+    return await axios.get('http://localhost:8080/api/v1.0/categories'); //maybe here the may cause error
 }
